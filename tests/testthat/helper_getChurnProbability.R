1c362995520b74833264ab1517d4fa4836f4f7c6
Uensal Satan <uensal4@gmail.com>
2021-02-12 11:33:42 +0100

    added factorization of exited and gender

--- tests/testthat/helper_getChurnProbability.R
+++ tests/testthat/helper_getChurnProbability.R
@@ -2,3 +2,6 @@ customer <- fread("data_customer.csv")
 personal <- fread("data_personal.csv")
 
 customer_personal <- merge(customer, personal, by = "CustomerId", all=FALSE)
+
+customer_personal[,Exited := as.factor(Exited)]
+customer_personal[,Gender := as.factor(Gender)]
--- tests/testthat/test-getChurnProbability.R
+++ tests/testthat/test-getChurnProbability.R
@@ -1,5 +1,3 @@
-
-
 test_that("getChurnProbability", {
   expect_gt(getChurnProbability(customer_personal, 15653251), getChurnProbability(customer_personal, 15662641))
 })
